6b371f34d8ac47fbdaa9a6701c65991e39b4a0ce
vikash4281 <peddinti.p@husky.neu.edu>
2017-09-19 04:18:35 -0400

    changes to name and css
    color added and font increased

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by vicky on 9/19/2017.
+ */
+$(document).ready(function () {
+    $(function(){
+        $("#profile-card-header").typed({
+            strings: ["Data Enthusiast",
+                "Web Designer",
+                "Software Developer",
+                "CS Graduate '18"
+            ],
+            typeSpeed: 0.4,
+            loop: true
+        });
+    });
+
+
+
+        });
